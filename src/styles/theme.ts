766b0263c31d57555fdae6b43175c843c870e984
Gabriel Santos <mfkrgabriel@gmail.com>
2021-02-13 15:01:55 -0300

    Add ThemeProvider

---
+++ src/styles/theme.ts
@@ -0,0 +1,44 @@
+export default {
+  grid: {
+    container: '190rem'
+  },
+  border: {
+    radius: '1rem'
+  },
+  font: {
+    family:
+      "Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 600,
+    bolder: 900,
+    sizes: {
+      xsmall: '0.75rem',
+      small: '0.875rem',
+      medium: '1rem',
+      large: '1.125rem',
+      xlarge: '1.25rem',
+      xxlarge: '1.375rem'
+    }
+  },
+  colors: {
+    black: '#000000',
+    white: '#FFFFFF'
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  }
+} as const
